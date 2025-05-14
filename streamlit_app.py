9358f95bd98cd447d5f4c62677cd8ea311ede15b
Faizanbb1 <faizan@gofindy.io>
2025-05-14 16:47:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -154,7 +154,7 @@ plain_english = f"""
 - **Financial Health Score:** {health_score:.0f}/100
 """
 
-col1, col2 = st.columns([1, 2])
+col1, col2 = st.columns([2, 1])
 
 with col2:
     st.subheader("🧠 Explain My Results")
